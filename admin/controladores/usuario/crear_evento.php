e7bd9ed0a29c38b3ac958d8502875964ad306515
Edw1nM4rquez <edwi1999@hotmail.com>
2019-11-23 16:23:44 -0500

    Editamos los esilos en css

---
+++ admin/controladores/usuario/crear_evento.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>Crear Nuevo Evento</title>
+ <style type="text/css" rel="stylesheet">
+ .error{
+ color: red;
+ }
+ </style>
+</head>
+<body>
+ <?php
+ 
+ //incluir conexión a la base de datos
+ include '../../config/conexionBDevento.php';
+ $asunto = isset($_POST["asunto"]) ? trim($_POST["asunto"]): null;
+ $fecha = isset($_POST["fecha"]) ? trim($_POST["fecha"]): null;
+
+ $hora = isset($_POST["hora"]) ? trim($_POST["hora"]) : null;
+
+ $lugar = isset($_POST["lugar"]) ? trim($_POST["lugar"]) : null;
+ $coordenadas = isset($_POST["cordenadas"]) ? trim($_POST["cordenadas"]): null;
+ $remitente = isset($_POST["remitente"]) ? trim($_POST["remitente"]): null;
+ $motivos = isset($_POST["motivos"]) ? trim($_POST["motivos"]): null;
+ $observaciones = isset($_POST["observaciones"]) ? trim($_POST["observaciones"]) : null;
+
+ $sql = "INSERT INTO usuario VALUES (0, '$fecha', '$asunto', '$hora', '$lugar', '$coordenadas',
+'$remitente',  '$motivos', '$observaciones', null)";
+
+ if ($conn->query($sql) === TRUE) {
+ echo "<p>Se ha creado un nuevo evento!!!</p>";
+ } else {
+ 
+ echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
+ 
+ }
+
+ //cerrar la base de datos
+ $conn->close();
+ echo "<a href='../../vista/usuario/crear_reuniones.html'>Regresar</a>";
+
+ ?>
+</body>
+</html>
